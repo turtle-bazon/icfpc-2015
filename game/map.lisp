3a8308497ea2d06270258bb8b81872c77162f657
Alexey Voznyuk <me@swizard.info>
2015-08-07 18:41:11 +0300

    Movement helpers and neighbours locating

--- game/map.lisp
+++ game/map.lisp
@@ -11,12 +11,29 @@
   nil)
 
 (defmethod cell ((obj hextris-map) row col)
-  (not (zerop (elt (field obj) (+ (* row (height obj)) col)))))
+  (when (and (>= row 0) (< row (height obj)) (>= col 0) (< col (width obj)))
+    (list row col (not (zerop (elt (field obj) (+ (* row (height obj)) col)))))))
 
 (defmethod (setf cell) (value (obj hextris-map) row col)
   (setf (elt (field obj) (+ (* row (height obj)) col))
         (if value 1 0))
-  obj)
+  (cell obj row col))
 
-;; (defmethod neighbours ((obj hextris-map) row col)
-  
+(defparameter *moves* '(:nw :ne :e :se :sw :w))
+
+(defmethod neighbour ((move (eql :nw)) (obj hextris-map) row col)
+  (cell obj (1- row) (if (evenp row) (1- col) col)))
+(defmethod neighbour ((move (eql :ne)) (obj hextris-map) row col)
+  (cell obj (1- row) (if (evenp row) col (1+ col))))
+(defmethod neighbour ((move (eql :e)) (obj hextris-map) row col)
+  (cell obj row (1+ col)))
+(defmethod neighbour ((move (eql :se)) (obj hextris-map) row col)
+  (cell obj (1+ row) (if (evenp row) col (1+ col))))
+(defmethod neighbour ((move (eql :sw)) (obj hextris-map) row col)
+  (cell obj (1+ row) (if (evenp row) (1- col) col)))
+(defmethod neighbour ((move (eql :w)) (obj hextris-map) row col)
+  (cell obj row (1- col)))
+
+(defmethod neighbours ((obj hextris-map) row col)
+  (remove-if-not #'identity (mapcar (lambda (move) (neighbour move obj row col)) *moves*)))
+      
