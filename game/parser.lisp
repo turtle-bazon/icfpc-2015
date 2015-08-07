bb0ea416fbcca08d085b90673fcaf922b28f66c2
Alexey Voznyuk <me@swizard.info>
2015-08-08 00:43:09 +0300

    parser fixed

--- game/parser.lisp
+++ game/parser.lisp
@@ -12,15 +12,13 @@
       (iter (for cell-json in (field :filled))
             (setf (map-cell parsed-map (make-cell :row (field :y cell-json) :col (field :x cell-json))) t))
       (let ((units (iter (for unit-json in (field :units))
-                         (for members = (iter (for member-json in (field :members unit-json))
-                                              (collect (make-cell :row (field :y member-json)
-                                                                  :col (field :x member-json)))))
                          (for pivot-json = (field :pivot unit-json))
                          (for pivot-row = (field :y pivot-json))
                          (for pivot-col = (field :x pivot-json))
-                         (collect (make-instance 'unit
-                                                 :pivot (make-cell :row pivot-row :col pivot-col)
-                                                 :members members)))))
+                         (for members = (iter (for member-json in (field :members unit-json))
+                                              (collect (make-cell :row (- (field :y member-json) pivot-row)
+                                                                  :col (- (field :x member-json) pivot-col)))))
+                         (collect (make-instance 'unit :members members)))))
         (make-instance 'game
                        :seeds (field :source-seeds)
                        :units units
