fb4df0ee45d78ea93e7a5b1213e1241f5bfb44ae
Alexey Voznyuk <me@swizard.info>
2015-08-08 02:16:45 +0300

    cube coords refactoring

--- game/coverage.lisp
+++ game/coverage.lisp
@@ -1,20 +1,12 @@
 
 (in-package :hextris)
 
-(defmethod translate-members ((obj unit-on-map) (field hextris-map))
-  (let* ((pivot-row (cell-row (unit-on-map-coord obj)))
-         (pivot-col (cell-col (unit-on-map-coord obj)))
-         (translated (mapcar (lambda (c) (make-cell :row (+ (cell-row c) pivot-row) :col (+ (cell-col c) pivot-col)))
-                             (members (unit-on-map-unit obj)))))
-    (when (every (lambda (c) (multiple-value-bind (found-cell filled-p) (map-cell field c) (and found-cell (not filled-p)))) translated)
-      translated)))
-
 (defmethod count-holes ((field hextris-map) row)
   (iter (with holes-count = 0)
         (with switch = :filled)
         (for col from 0 below (width field))
         (multiple-value-bind (cell filled-p)
-            (map-cell field (make-cell :row row :col col))
+            (map-cell field (make-cell-row-col row col))
           (declare (ignore cell))
           (if filled-p
               (ecase switch
@@ -30,14 +22,18 @@
         (for best-position = 
              (iter outer-loop
                    (for col from 0 below (width field))
-                   (iter (with installed-unit = (make-unit-on-map :unit sample-unit
-                                                                  :coord (make-cell :row row :col col)))
+                   (iter (with installed-unit = (make-unit-on-map :unit sample-unit :coord (make-cell-row-col row col)))
                          (repeat 6) ;;; rotate six times
-                         (for translated-members = (translate-members installed-unit field))
-                         (when translated-members
+                         (for translated = (place-on-map (unit-on-map-unit installed-unit)
+                                                         (unit-on-map-coord installed-unit)
+                                                         field))
+                         (when translated
                            (in outer-loop
-                               (finding installed-unit minimizing (count-holes (unit-lock (unit-on-map-unit installed-unit) field)
-                                                                               (1- (height field))))))
+                               (finding installed-unit
+                                        minimizing (count-holes (unit-lock (unit-on-map-unit installed-unit)
+                                                                           (unit-on-map-coord installed-unit)
+                                                                           field)
+                                                                (1- (height field))))))
                          (setf installed-unit (cdr (move-unit :rotate-cw installed-unit field))))))
         (when best-position
           (return-from locate-target best-position))))
