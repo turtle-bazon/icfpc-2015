d5817452e5ef2c5fb5132a7728dd4b4ea2dfb1d3
Alexey Voznyuk <me@swizard.info>
2015-08-07 19:02:30 +0300

    Parsing filled cells support

--- game/parser.lisp
+++ game/parser.lisp
@@ -7,8 +7,10 @@
      (json:decode-json in))))
 
 (defun parse-input-json (json)
-  (flet ((field (name) (cdr (assoc name json))))
-    (make-instance 'game
-                   :seeds (field :source-seeds)
-                   :map (make-instance 'hextris-map :width (field :width) :height (field :height)))))
-  
+  (flet ((field (name &optional (base json)) (cdr (assoc name base))))
+    (let ((parsed-map (make-instance 'hextris-map :width (field :width) :height (field :height))))
+      (iter (for map-cell in (field :filled))
+            (setf (cell parsed-map (field :y map-cell) (field :x map-cell)) t))
+      (make-instance 'game
+                     :seeds (field :source-seeds)
+                     :map parsed-map))))
