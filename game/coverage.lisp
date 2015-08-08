f3529163cdc87d5bf7101d4cb6b4f37657cb605a
Alexey Voznyuk <me@swizard.info>
2015-08-08 04:50:56 +0300

    union coverage with a-star

--- game/coverage.lisp
+++ game/coverage.lisp
@@ -23,25 +23,29 @@
         (counting (multiple-value-bind (cell filled-p) (map-cell field cell)
                     (and cell (not filled-p))))))
   
-(defmethod locate-target ((field hextris-map) (sample-unit unit))
+(defmethod locate-target ((field hextris-map) (initial-unit unit-on-map))
   (iter (for row from (1- (height field)) downto 0)
         (iter (for col from 0 below (width field))
-              (for best-position =
-                   (iter (with installed-unit = (make-unit-on-map :unit sample-unit :coord (make-cell-row-col row col)))
+              (for best-choice =
+                   (iter (with installed-unit = (make-unit-on-map :unit (unit-on-map-unit initial-unit)
+                                                                  :coord (make-cell-row-col row col)))
                          (repeat 6) ;;; rotate six times
                          (for translated = (place-on-map (unit-on-map-unit installed-unit)
                                                          (unit-on-map-coord installed-unit)
                                                          field))
                          (when translated
-                           (for lowest-row = (iter (for m in (members (unit-on-map-unit installed-unit)))
-                                                   (multiple-value-bind (row col) (cell-row-col m)
-                                                     (declare (ignore col))
-                                                     (minimizing row))))
-                           (finding installed-unit
-                                    minimizing (count-free-cells lowest-row
-                                                                 (unit-lock (unit-on-map-unit installed-unit)
-                                                                            (unit-on-map-coord installed-unit)
-                                                                            field))))
+                           (multiple-value-bind (reachable-p move-script)
+                               (run-a-star field initial-unit installed-unit)
+                             (when reachable-p
+                               (for lowest-row = (iter (for m in (members (unit-on-map-unit installed-unit)))
+                                                       (multiple-value-bind (row col) (cell-row-col m)
+                                                         (declare (ignore col))
+                                                         (minimizing row))))
+                               (finding (list installed-unit move-script)
+                                        minimizing (count-free-cells lowest-row
+                                                                     (unit-lock (unit-on-map-unit installed-unit)
+                                                                                (unit-on-map-coord installed-unit)
+                                                                                field))))))
                          (setf installed-unit (cdr (move-unit :rcw installed-unit field)))))
-              (when best-position
-                (return-from locate-target best-position)))))
+              (when best-choice
+                (return-from locate-target best-choice)))))
