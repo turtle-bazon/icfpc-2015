16f608b98c69f76215c4b2237ebc6187e398d560
Azamat S. Kalimoulline <turtle@bazon.ru>
2015-08-10 03:28:10 +0300

    Score output.

--- game/main.lisp
+++ game/main.lisp
@@ -24,9 +24,14 @@
                     (string= "true" (string-downcase pvalue))
                     (string= "yes" (string-downcase pvalue)))
             (for publish = t)))
+        (when (string= "-score-only" (string-downcase pname))
+          (when (or (string= "t" (string-downcase pvalue))
+                    (string= "true" (string-downcase pvalue))
+                    (string= "yes" (string-downcase pvalue)))
+            (for score-only = t)))
         (when (string= "-tag" (string-downcase pname))
           (for tag = (string-downcase pvalue)))
-        (finally (return (list files time-limit memory-limit number-cores phrases publish tag)))))
+        (finally (return (list files time-limit memory-limit number-cores phrases publish score-only tag)))))
 
 (defun execution-plan (files)
   (iter (for fname in files)
@@ -45,7 +50,8 @@
       (lambda ()
         (sb-sys:with-interrupts
           (sb-ext:exit)) ()))))
-  (destructuring-bind (files time-limit memory-limit number-cores phrases publish tag)
+  (destructuring-bind (files time-limit memory-limit number-cores phrases
+                             publish score-only tag)
       (parse-args (rest args))
     (let ((*power-phrases* phrases)
           (solutions '())
@@ -68,23 +74,33 @@
                                        (bordeaux-threads:with-lock-held (solutions-lock)
                                          (push result solutions)))))))
       (thread-pool:stop-pool tpool)
-      (let ((result-string (json:encode-json-to-string
-                            (iter (for s in solutions)
-                                  (for game = (getf s :game))
-                                  (for seed = (getf s :seed))
-                                  (for script = (getf s :script))
-                                  (collecting `((:problem-id . ,(problem-id game))
-                                                (:seed . ,seed)
-                                                (:solution . ,(power-phrase-encode-adt script))
-                                                ,@(when tag `((:tag . ,(format nil "~a-~a-~a"
-                                                                              tag
-                                                                              (problem-id game)
-                                                                              (get-internal-real-time)))))))))))
-        (if publish
-            (format t "~a~&"
-                    (http-request *publish-api-uri*
-                                  :method :post
-                                  :basic-authorization `("" ,*publish-api-key*)
-                                  :content-type "application/json"
-                                  :content result-string)))
-        (format t "~a~&" result-string)))))
+      (if score-only
+          (iter (for s in (sort solutions #'>
+                                :key (lambda (v)
+                                       (+ (* 100 (problem-id (getf v :game)))
+                                          (getf v :seed)))))
+                (for game = (getf s :game))
+                (for seed = (getf s :seed))
+                (for score = (getf s :score))
+                (format t "G: ~a, S: ~a, Sc: ~a~&"
+                        (problem-id game) seed score))
+          (let ((result-string (json:encode-json-to-string
+                                (iter (for s in solutions)
+                                      (for game = (getf s :game))
+                                      (for seed = (getf s :seed))
+                                      (for script = (getf s :script))
+                                      (collecting `((:problem-id . ,(problem-id game))
+                                                    (:seed . ,seed)
+                                                    (:solution . ,(power-phrase-encode-adt script))
+                                                    ,@(when tag `((:tag . ,(format nil "~a-~a-~a"
+                                                                                   tag
+                                                                                   (problem-id game)
+                                                                                   (get-internal-real-time)))))))))))
+            (if publish
+                (format t "~a~&"
+                        (http-request *publish-api-uri*
+                                      :method :post
+                                      :basic-authorization `("" ,*publish-api-key*)
+                                      :content-type "application/json"
+                                      :content result-string)))
+            (format t "~a~&" result-string))))))
